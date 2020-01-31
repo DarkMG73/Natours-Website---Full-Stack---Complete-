2c332d569382de7af9dd216140d8e93280398581
DarkMG73 <levelthreeemail@gmail.com>
2020-01-30 23:43:51 -0500

    update

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -24,7 +24,7 @@ exports.getTour = catchAsync(async (req, res) => {
 
   // 3) Render template usingdata
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: `${tour.name} Tour`,
     tour
   });
 });
